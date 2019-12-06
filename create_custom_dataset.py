5cf22ab10b82dc1ec389759aa62e83066ac2fa44
Nafisa <amirnafisa@gmail.com>
2019-12-06 12:48:28 -0500

    Modified custom_dataset python file and adding outsource utility

--- create_custom_dataset.py
+++ create_custom_dataset.py
@@ -15,106 +15,12 @@
 # - - - - - - - - - - - - > crd/labels/train_<imageID>.txt
 # - - - - - - - - - - - - > crd/labels/validation_<imageID>.txt
 # - - - - - - - - - - - - > crd/labels/test_<imageID>.txt
-#Rewrites ../data/coco.names
 import requests
 import csv
 import urllib.request
 import codecs
 import os
-
-#globals
-subsets = ['train', 'val', 'test']
-classNames = ['Door handle','Light switch']
-
-MIDs = 'https://storage.googleapis.com/openimages/v5/class-descriptions-boxable.csv'
-
-annotations = {}
-annotations['train'] = 'https://storage.googleapis.com/openimages/2018_04/train/train-annotations-bbox.csv'
-annotations['val'] = 'https://storage.googleapis.com/openimages/v5/validation-annotations-bbox.csv'
-annotations['test'] = 'https://storage.googleapis.com/openimages/v5/test-annotations-bbox.csv'
-
-
-imageURLs = {}
-imageURLs['train'] = 'https://storage.googleapis.com/openimages/2018_04/train/train-images-boxable-with-rotation.csv'
-imageURLs['val'] = 'https://storage.googleapis.com/openimages/2018_04/validation/validation-images-with-rotation.csv'
-imageURLs['test'] = 'https://storage.googleapis.com/openimages/2018_04/test/test-images-with-rotation.csv'
-
-coco_names = '../yolov3/data/coco.names'
-
-def getMIDs(url, classNames):
-    labelMID = {}
-
-    ftpstream = urllib.request.urlopen(url)
-    csvfile = csv.reader(codecs.iterdecode(ftpstream, 'utf-8'))
-    for lineContents in csvfile:
-        if lineContents[1] in classNames:
-            labelMID[lineContents[1]] = lineContents[0] 
-
-    return labelMID
-
-
-def getImageIDsFromMID(url, matchMID):
-
-    imageIDs = {}
-
-    ftpstream = urllib.request.urlopen(url)
-    csvfile = csv.reader(codecs.iterdecode(ftpstream, 'utf-8'))
-    next(csvfile)
-    for lineContents in csvfile:
-        imageID = lineContents[0]
-        MID = lineContents[2]
-        [left, right, top, bot] = [float(lineContents[4]), float(lineContents[5]), float(lineContents[6]), float(lineContents[7])]
-        if MID == matchMID:
-            imageIDs[imageID] = [left, right, top, bot]
-
-    return imageIDs
-
-
-def getImageURLsFromIDs(url, imageIDs):
-    
-    ftpstream = urllib.request.urlopen(url)
-    csvfile = csv.reader(codecs.iterdecode(ftpstream, 'utf-8'))
-    next(csvfile)
-    for lineContents in csvfile:
-        imageID = lineContents[0]
-        imageURL = lineContents[2]
-        if imageID in imageIDs:
-            imageIDs[imageID].append(imageURL)
-
-    return imageIDs 
-
-def downloadImages(subset, classNameIndex, imageIDs):
-    for imageID, [left, right, top, bot, imageURL] in imageIDs.items():
-
-        filePath = 'images/'+subset+'2014/'+imageID+'.jpg'
-        with open(filePath, 'wb') as handle:
-            response = requests.get(imageURL, stream=True)
-
-            if not response.ok:
-                print(response)
-                print("Removing "+filePath)
-                os.remove(filePath)
-                continue
-            for block in response.iter_content(1024):
-                if not block:
-                    break
-
-                handle.write(block)
-        print("Downloaded "+imageID+" from "+imageURL)
-                
-        x_center = str(left + ((right - left)/2))
-        y_center = str(top + ((bot - top)/2))
-        width = str(right - left)
-        height = str(bot - top)
-        classNameIndex = str(classNameIndex)
-
-        with open('labels/'+subset+'2014/'+imageID+'.txt', 'w') as file:
-            file.write(classNameIndex+' '+x_center+' '+y_center+' '+width+' '+height+'\n')
-
-def rewrite_coco_names(file):
-    with open(file, "w") as f:
-        for className in classNames:
-            f.write(className+'\n')
+from tqdm import tqdm
 
 def create_directory_structure(directories):
    
@@ -123,27 +29,132 @@ def create_directory_structure(directories):
             os.makedirs(directory)
 
 
-def sanity_check(subset, imageIDs):
-    for imageID in imageIDs.keys():
+def sanity_check(imageIDs):
+    for subset, info in imageIDs.items():
+        for imageID in info.keys():
+            filePath = os.path.join(subset,'images',imageID+'.jpg')
+            labelfilePath = os.path.join(subset,'labels',imageID+'.txt')
+            if (os.path.exists(filePath)):
+                if os.path.getsize(filePath) == 0:
+                    print(filePath)
+                    os.remove(filePath)
+                    if (os.path.exists(labelfilePath)):
+                        print(labelfilePath)
+                        os.remove(labelfilePath)
 
-        filePath = 'images/'+subset+'2014/'+imageID+'.jpg'
-        if (os.path.exists(filePath)):
-            if os.path.getsize(filePath) == 0:
-                print(filePath)
-                os.remove(filePath)
+
+def downloadImages(imageInfo):
+    for subset, details in imageInfo.items():
+        total_downloads = 1
+        n_downloads = [0, 0, 0]
+        for imageID, info in details.items():
+            if n_downloads[info['labels'][0][0]] < 600:
+                n_downloads[info['labels'][0][0]] = n_downloads[info['labels'][0][0]] + 1
+
+                filePath = os.path.join(subset,'images',imageID+'.jpg')
+                with open(filePath, 'wb') as handle:
+                    response = requests.get(info['url'], stream=True)
+                    if not response.ok:
+                        print(response)
+                        print("Removing "+filePath)
+                        os.remove(filePath)
+                        continue
+                    for block in response.iter_content(1024):
+                        if not block:
+                            break
+
+                        handle.write(block)
+            
+                print(total_downloads,"Downloaded "+imageID+" from "+info['url'])
+                filePath = os.path.join(subset,'labels',imageID+'.txt')
+                with open(filePath,'w') as file:
+                    for [category, x_min, y_min, x_max, y_max] in info['labels']:
+                        file.write(str(category)+' '+str(x_min)+' '+str(y_min)+' '+str(x_max)+' '+str(y_max)+'\n')
+                        idx = category
+
+                print("Individual Downloads:",n_downloads)
+                total_downloads += 1
+
+
+
+def extractImageURLsFromIDs(URLs, imageIDs):
+    print("Extracting Image URLs")
+    for subset, url in URLs.items():
+        print("Working on subset: ",subset)
+        ftpstream = urllib.request.urlopen(url)
+        csvfile = csv.reader(codecs.iterdecode(ftpstream, 'utf-8'))
+        next(csvfile)
+        for lineContents in tqdm(csvfile):
+            imageID = lineContents[0]
+            imageURL = lineContents[2]
+            if imageID in imageIDs[subset]:
+                imageIDs[subset][imageID]['url'] = imageURL
+    print("Done.")
+    return imageIDs 
+
+def extractImageIDs(URLs, categories):
+    print("Extracting Image Ids...")
+    imageIDs = {}
+    
+
+    for subset, url in URLs.items():
+        print("Working on subset: ",subset)
+        if subset not in imageIDs:
+            imageIDs[subset] = {}
+
+        ftpstream = urllib.request.urlopen(url)
+        csvfile = csv.reader(codecs.iterdecode(ftpstream, 'utf-8'))
+        next(csvfile)
+        for lineContents in tqdm(csvfile):
+            imageID = lineContents[0]
+            category = lineContents[2]
+            [x_min, x_max, y_min, y_max] = [float(lineContents[4]), float(lineContents[5]), float(lineContents[6]), float(lineContents[7])]
+            if category in categories:
+                if imageID not in imageIDs[subset]:
+                    imageIDs[subset][imageID] = {}
+                    imageIDs[subset][imageID]['labels'] = []
+                imageIDs[subset][imageID]['labels'].append([categories.index(category), x_min, y_min, x_max, y_max])
+
+    print("Done.")
+    return imageIDs
+
+def extractMIDs(url, objects):
+    print("Extracting MIDs...")
+    categories = []
+
+    ftpstream = urllib.request.urlopen(url)
+    csvfile = csv.reader(codecs.iterdecode(ftpstream, 'utf-8'))
+    for lineContents in tqdm(csvfile):
+        if lineContents[1] in objects:
+            categories.append(lineContents[0])
+    
+    print("Done.")
+    return categories 
+
 
 if __name__ == "__main__":
-    directories = ['images','images/train2014/','images/val2014','images/test2014','labels','labels/train2014','labels/val2014','labels/test2014']
+
+    directories = ['train','val','test','train/images','val/images','test/images','train/labels','val/labels','test/labels']
+    
+    classNames = ["Door handle, Person, Light switch"]
+
+    MID_url = 'https://storage.googleapis.com/openimages/v5/class-descriptions-boxable.csv'
+    annotations = {}
+    annotations['train'] = 'https://storage.googleapis.com/openimages/2018_04/train/train-annotations-bbox.csv'
+    annotations['val'] = 'https://storage.googleapis.com/openimages/v5/validation-annotations-bbox.csv'
+    annotations['test'] = 'https://storage.googleapis.com/openimages/v5/test-annotations-bbox.csv'
+
+    imageURLs = {}
+    imageURLs['train'] = 'https://storage.googleapis.com/openimages/2018_04/train/train-images-boxable-with-rotation.csv'
+    imageURLs['val'] = 'https://storage.googleapis.com/openimages/2018_04/validation/validation-images-with-rotation.csv'
+    imageURLs['test'] = 'https://storage.googleapis.com/openimages/2018_04/test/test-images-with-rotation.csv'
+
+
+
     create_directory_structure(directories)
-
-    labelMID = getMIDs(MIDs, classNames)
-    for classNameIndex, className in enumerate(classNames):
-        for subset in subsets:
-            print("Working on " + className + " for " + subset)
-            imagesData = getImageIDsFromMID(annotations[subset], labelMID[className])
-            imagesData = getImageURLsFromIDs(imageURLs[subset], imagesData)
-            downloadImages(subset, classNameIndex, imagesData)
-            sanity_check(subset, imagesData)
-
-    rewrite_coco_names(coco_names)
+    categories = extractMIDs(MID_url, classNames)
+    images_info = extractImageIDs(annotations, categories)
+    images_info = extractImageURLsFromIDs(imageURLs, images_info)
+    downloadImages(images_info)
+    sanity_check(images_info)
 
